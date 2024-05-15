ec74340d5026cc9b17edcbb174650d145ba906be
leocerqueirasilva <120220664+leocerqueirasilva@users.noreply.github.com>
2024-05-15 13:11:42 -0300

    Commit

--- packages/embeds/nextjs/.eslintrc.cjs
+++ packages/embeds/nextjs/.eslintrc.cjs
@@ -5,6 +5,6 @@ module.exports = {
     '@next/next/no-img-element': 'off',
     '@next/next/no-html-link-for-pages': 'off',
     "react/no-unescaped-entities": "off",
-    "@next/next/no-page-custom-font": "off"
+    "@next/next/no-page-custom-font": "off",
   },
 }
